01e96ecd7a99538accf4e20a28dc5a2dddc5365d
kitjsr <kitjsr2021@gmail.com>
2023-07-12 08:55:26 +0530

    initial setup

---
+++ roles.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 11, 2023 at 09:12 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
+(1, 'student', '2023-06-11 09:11:28', '2023-06-11 09:11:28'),
+(2, 'parent', '2023-06-11 09:11:28', '2023-06-11 09:11:28'),
+(3, 'teacher', '2023-06-11 09:11:57', '2023-06-11 09:11:57'),
+(4, 'admin', '2023-06-11 09:11:57', '2023-06-11 09:11:57');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+// Incoming PORT details
+var corsOptions = {
+  origin: "http://localhost:4200"
+};
+
+// let corsOptions = {
+//   origin: (origin, callback)=>{
+//       if (['http://localhost:4200'].indexOf(origin) !== -1) {
+//           callback(null, true)
+//       } else {
+//           callback(new Error('Not allowed by CORS'))
+//       }
+//   },credentials: true
+// }
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+
+db.sequelize.sync();
+ 
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Our Node Server" });
+});
+
+// routes: 
+require('./app/routes/auth.routes')(app);
+require("./app/routes/user.routes")(app);
+require("./app/routes/student.routes")(app);
+require("./app/routes/staff.routes")(app);
+require("./app/routes/at.routes")(app);
+require("./app/routes/notice.routes")(app);
+require("./app/routes/expense.routes")(app);
+require("./app/routes/tc.routes")(app);
+require("./app/routes/classteacher.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
